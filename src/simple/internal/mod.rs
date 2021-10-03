ea313dcc129f51fd02468b1dd027b291d552d2ea
Dylan Bowker <dylan.bowker@arkiteq.ca>
2021-10-02 18:50:14 -0600

    Adjusted internal table size.

--- src/simple/internal/mod.rs
+++ src/simple/internal/mod.rs
@@ -6,5 +6,5 @@ mod string_keywords;
 
 // -----------------------------------------------------------------------------
 
-const MAXIMUM_INTERNAL_AUTOCOMPLETE_RESULTS: usize = 20_560;
-const MAXIMUM_INTERNAL_SEARCH_RESULTS: usize = 20_560;
+const MAXIMUM_INTERNAL_AUTOCOMPLETE_RESULTS: usize = 40_960;
+const MAXIMUM_INTERNAL_SEARCH_RESULTS: usize = 40_960;
